488278766ca143d9fc5bad6a0a5d864b5d8cd8c4
Shivanand <shivanandsunagar1203@gmail.com>
2022-04-27 18:17:29 +0530

    changed login page image

--- student_profiling/settings.py
+++ student_profiling/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_jx#q=vf!02^xey*y=f$v435f0k0_cx*)a6)3vy=h5)6+4gswh'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -133,10 +133,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'student/static'),
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static'),
 
-]
+# ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
