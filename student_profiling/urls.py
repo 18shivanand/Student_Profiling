66551303e196333b92153f0b12c9fff8e5cc00f2
SHIVANAND <83900498+18shivanand@users.noreply.github.com>
2022-07-04 20:16:14 +0530

    added the url

--- student_profiling/urls.py
+++ student_profiling/urls.py
@@ -21,11 +21,16 @@ from django.conf.urls.static import  static
 from django.conf import settings
 from chartjs import views
 
+from django.views.static import serve
+from django.conf.urls import url
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('student.urls')),
     path('profile/', include('resume.urls')),
     path('api', views.ChartData.as_view()),
     path('chart', views.HomeView.as_view()),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ] 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
