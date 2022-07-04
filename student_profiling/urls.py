32aa711d25e24713f7acf7d81a0aed7bfbeb59bb
SHIVANAND <83900498+18shivanand@users.noreply.github.com>
2022-07-04 20:32:19 +0530

    nothing

--- student_profiling/urls.py
+++ student_profiling/urls.py
@@ -22,7 +22,6 @@ from django.conf import settings
 from chartjs import views
 
 from django.views.static import serve
-from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -30,7 +29,7 @@ urlpatterns = [
     path('profile/', include('resume.urls')),
     path('api', views.ChartData.as_view()),
     path('chart', views.HomeView.as_view()),
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
+    path(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ] 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
